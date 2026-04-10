93c22fcc6a92344164a7cd5bf685c1a740c8d3c9
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-04-10 20:58:22 +0530

    Day-39 Getting Started with Multithreading

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello from java multithreading...");
+        System.out.println(Thread.currentThread().getName());
     }
 }

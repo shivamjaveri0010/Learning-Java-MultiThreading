f08e32113ed3399424b8a3d41b494880d5d91ac3
shivamjaveri0010 <shivamjaveri0010@gmail.com>
2026-04-11 12:30:08 +0530

    Day-41 Synchronization and Race Condition

---
+++ Counter.java
@@ -0,0 +1,14 @@
+public class Counter {
+
+    private int count = 0;
+
+    public void increment() {
+        synchronized (this) {
+            count ++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
--- MyThread.java
+++ MyThread.java
@@ -1,26 +1,15 @@
 public class MyThread extends Thread {
-    
-    @Override
-    public void run() {
-        System.out.println("RUNNING");
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println(e);
-        }
+
+    private Counter counter;
+
+    public MyThread(Counter counter) {
+        this.counter = counter;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        System.out.println(t1.getState());
-
-        t1.start();
-        System.out.println(t1.getState());
-
-        Thread.sleep(100);
-        System.out.println(t1.getState());
-
-        t1.join();
-        System.out.println(t1.getState());
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        } 
     }
 }
